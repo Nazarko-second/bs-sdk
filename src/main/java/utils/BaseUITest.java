f416eca04d7c8bc102d6f56ec4feb07f28f2bff4
pc-ndovhoshyya <nazar.dovhoshyya@pharmacann.com>
2024-12-12 12:16:36 -0600

    base classes added

---
+++ src/main/java/utils/BaseUITest.java
@@ -0,0 +1,34 @@
+package utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import java.lang.reflect.Method;
+
+
+public class BaseUITest extends BaseTest{
+
+    private static final Logger logger = LoggerFactory.getLogger(BaseUITest.class);
+
+    @BeforeMethod(alwaysRun = true)
+    public void beforeWithData(Object[] data, Method method) {
+        logger.info("BeforeMethod in Base UI Test");
+        super.beforeWithData(data, method);
+        logger.info("CONFIG_DATA in BaseUITest");
+        logger.info("qTest integration: {}", System.getProperty("qTest"));
+        logger.info("Config: {}", System.getProperty("config"));
+        logger.info("Groups: {}", System.getProperty("groups"));
+        logger.info("browserstack.config: {}", System.getProperty("browserstack.config"));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void endTest(ITestResult testResult, Method method) {
+        logger.info("Start of AfterMethod in Base UI Test");
+        // do stuff
+        super.endTest(testResult, method);
+        logger.info("End of AfterMethod in Base UI Test");
+    }
+
+}
